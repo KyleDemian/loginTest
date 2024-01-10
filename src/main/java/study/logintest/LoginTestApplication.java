6ae7d1a8cb1964ed6ddf8ae73b79544791f7176f
air <air@lush.co.kr>
2024-01-10 15:44:35 +0900

    ?

--- src/main/java/study/logintest/LoginTestApplication.java
+++ src/main/java/study/logintest/LoginTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginTestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoginTestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LoginTestApplication.class, args);
+	}
 
 }
